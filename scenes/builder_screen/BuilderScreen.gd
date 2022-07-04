67a68444b418c4823241b655ff5f794e5040264c
Anthony Aromin <menekoiskawaii@gmail.com>
2022-07-04 18:52:54 +0800

    Refactor camera rotation in Builder Screen

--- scenes/builder_screen/BuilderScreen.gd
+++ scenes/builder_screen/BuilderScreen.gd
@@ -1,23 +1,26 @@
 extends Node
 
-var scene_camera: Spatial
 
-var mouse_is_pressed: bool = false
-var mouse_initial_position: Vector2 = Vector2(0, 0)
+var _mouse_is_pressed: bool = false
+var _mouse_initial_position: Vector2 = Vector2(0, 0)
 
-func _ready():
-	scene_camera = get_node("CameraHelper")
+onready var _camera_helper: Spatial = get_node("CameraHelper")
+onready var _camera: Camera = get_node("CameraHelper/Camera")
 
 
-func _input(event):
-	if (event is InputEventMouseButton):
+func _input(event) -> void:
+	if event is InputEventMouseButton:
 		if event.is_pressed():
-			mouse_is_pressed = true
-			mouse_initial_position = event.position
+			_mouse_is_pressed = true
+			_mouse_initial_position = event.position
 		else:
-			mouse_is_pressed = false
-			mouse_initial_position = Vector2(0, 0)
+			_mouse_is_pressed = false
+			_mouse_initial_position = Vector2(0, 0)
 
-	if (event is InputEventMouseMotion and mouse_is_pressed):
-		var distance = event.position - mouse_initial_position
-		scene_camera.rotate_y(distance.x / -7000)
+	if event is InputEventMouseMotion and _mouse_is_pressed:
+		var rotation_value = deg2rad(event.position.x - _mouse_initial_position.x) / -200
+		_rotate_camera(rotation_value)
+
+
+func _rotate_camera(value) -> void:
+	_camera_helper.rotate_y(value)
