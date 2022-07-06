f72e0078e470abc2945b11bdc4433f3ee303d25e
Anthony Aromin <menekoiskawaii@gmail.com>
2022-07-07 00:26:50 +0800

    Fix a bug preventing cage walls to be drawn along Z

--- objects/Cage.gd
+++ objects/Cage.gd
@@ -4,7 +4,7 @@ class_name Cage
 
 func build_wall(from: Vector3, to: Vector3) -> void:
 	if from.x == to.x:
-		var _range: Range = Utils.min_max_range(from.x, to.x)
+		var _range: Array = Utils.min_max_range(from.z, to.z)
 
 		for i in _range:
 			_add_wall(
@@ -12,7 +12,7 @@ func build_wall(from: Vector3, to: Vector3) -> void:
 				Vector3(from.x, from.y, i + 1)
 			)
 	elif from.z == to.z:
-		var _range = Utils.min_max_range(from.x, to.x)
+		var _range: Array = Utils.min_max_range(from.x, to.x)
 
 		for i in _range:
 			_add_wall(
