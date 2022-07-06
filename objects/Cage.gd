5e40b1747e04eff6bf27bcb6900a390f78307196
Anthony Aromin <menekoiskawaii@gmail.com>
2022-07-07 00:09:19 +0800

    Add feature to draw cage walls

--- objects/Cage.gd
+++ objects/Cage.gd
@@ -2,5 +2,26 @@ extends Spatial
 class_name Cage
 
 
-func _ready():
-	pass
+func build_wall(from: Vector3, to: Vector3) -> void:
+	if from.x == to.x:
+		var _range: Range = Utils.min_max_range(from.x, to.x)
+
+		for i in _range:
+			_add_wall(
+				Vector3(from.x, from.y, i),
+				Vector3(from.x, from.y, i + 1)
+			)
+	elif from.z == to.z:
+		var _range = Utils.min_max_range(from.x, to.x)
+
+		for i in _range:
+			_add_wall(
+				Vector3(i, from.y, from.z),
+				Vector3(i + 1, from.y, from.z)
+			)
+
+
+func _add_wall(from: Vector3, to: Vector3) -> void:
+	var new_panel: Wall = Wall.new(from, to)
+
+	add_child(new_panel)
